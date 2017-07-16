f9ba3e3398c1b7c9a00de95568b0022bfc8ff98f
kreegan <rharkrader@gmail.com>
2017-07-16 16:49:12 -0400

    Fleshing out merchant shop generation. Added some data files.

--- src/main/java/io/github/kreegan/arcfire/model/InventoryItem.java
+++ src/main/java/io/github/kreegan/arcfire/model/InventoryItem.java
@@ -9,4 +9,36 @@ public class InventoryItem {
     private String description;
     private Double cost;
     private Integer weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
 }
--- src/test/java/io/github/kreegan/arcfire/model/MerchantShopTest.java
+++ src/test/java/io/github/kreegan/arcfire/model/MerchantShopTest.java
@@ -2,10 +2,7 @@ package io.github.kreegan.arcfire.model;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -32,5 +29,117 @@ public class MerchantShopTest {
         assertNotNull(merchantShop.getInventory());
         assertNotNull(merchantShop.getProprietors());
         assertTrue(merchantShop.getHasMagicItems());
+    }
+
+    @Test
+    public void generateDwarfClanNames() {
+        List<String> prefixes = Arrays.asList(
+                "Stone",
+                "Steel",
+                "Iron",
+                "Dark",
+                "Fire",
+                "Crag",
+                "Storm",
+                "Thunder",
+                "Mead",
+                "Molten",
+                "Mountain",
+                "Orc",
+                "Red",
+                "Gray",
+                "Silver",
+                "Black",
+                "Rock",
+                "Rune",
+                "Stout",
+                "Strong",
+                "Battle",
+                "Grim",
+                "Horn",
+                "Ice",
+                "Giant",
+                "Dire",
+                "Frost",
+                "Burning",
+                "Blood",
+                "Cold",
+                "Deep",
+                "Grey",
+                "Flint",
+                "Swift",
+                "Dour",
+                "Earth",
+                "Earthen",
+                "Fleet",
+                "Soot",
+                "Coal",
+                "Snow",
+                "Copper",
+                "Silver",
+                "Pillar",
+                "Anvil",
+                "Gold",
+                "Nether",
+                "White"
+                );
+        List<String> suffixes = Arrays.asList(
+                "mail",
+                "stone",
+                "forge",
+                "born",
+                "eye",
+                "blade",
+                "axe",
+                "hammer",
+                "bane",
+                "breaker",
+                "delve",
+                "brew",
+                "thew",
+                "beard",
+                "brow",
+                "spike",
+                "braid",
+                "deep",
+                "helm",
+                "hew",
+                "spine",
+                "striker",
+                "grip",
+                "caller",
+                "forged",
+                "heart",
+                "fist",
+                "arm",
+                "foot",
+                "boot",
+                "bellow",
+                "brand",
+                "fury",
+                "rage",
+                "foe",
+                "tooth",
+                "breath",
+                "heel",
+                "hand",
+                "guide",
+                "gate",
+                "soul",
+                "silver",
+                "pillar",
+                "gold",
+                "pike",
+                "pick",
+                "mine",
+                "miner",
+                "warden"
+                );
+
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < 200; i++) {
+            String name = prefixes.get(random.nextInt(prefixes.size())) + suffixes.get(random.nextInt(suffixes.size()));
+            System.out.println(name);
+        }
     }
 }
